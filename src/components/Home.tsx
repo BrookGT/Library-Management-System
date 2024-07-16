ddf5da7404fbfd7eee20ce881cd9d812ea1cc6ed
Biruk GT <birukgebisa7@gmail.com>
2024-07-16 14:14:09 +0300

    create login pages

---
+++ src/components/Home.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { Box, Button, Heading, HStack, Text, VStack } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import backgroundImage from "../assets/background.jpg";
+
+const Home: React.FC = () => {
+    return (
+        <Box
+            bgImage={`url(${backgroundImage})`}
+            bgSize="cover"
+            bgPosition="center"
+            minH="100vh"
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+        >
+            <VStack
+                spacing={4}
+                bg="rgba(255, 255, 255, 0.8)"
+                p={8}
+                borderRadius="md"
+            >
+                <Heading>Welcome to the Library Management System</Heading>
+                <Text>
+                    If you don't have an account, please sign up. If you already
+                    have an account, please sign in.
+                </Text>
+                <HStack>
+                    <Button as={Link} to="/signup" colorScheme="teal">
+                        Sign Up
+                    </Button>
+                    <Button as={Link} to="/signin" colorScheme="teal">
+                        Sign In
+                    </Button>
+                    <Button as={Link} to="/admin-login" colorScheme="teal">
+                        Admin
+                    </Button>
+                </HStack>
+            </VStack>
+        </Box>
+    );
+};
+
+export default Home;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+// src/main.tsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ChakraProvider } from "@chakra-ui/react";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const root = ReactDOM.createRoot(
+    document.getElementById("root") as HTMLElement
+);
+root.render(
+    <React.StrictMode>
+        <ChakraProvider>
+            <App />
+        </ChakraProvider>
+    </React.StrictMode>
+);
